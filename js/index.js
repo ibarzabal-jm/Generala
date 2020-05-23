4bea6777c6a8818a97e216d950dc9d141e9e3a16
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-05-23 16:09:46 -0300

    Update index.js
    Se me ocurrio mostrarlo por pantalla cuando toque el boton, y que lo haga reiteradas veces.

--- js/index.js
+++ js/index.js
@@ -1,70 +1,100 @@
+let btnJugar = document.getElementById('btn-jugar');
+btnJugar.addEventListener('click', jugar);
+
+
+function saludar(){
+	console.log('Hola')
+}
+
+
 function getDado(){
-
-	
+		
 	return Math.floor( Math.random() * 6 + 1);
-
 }
 
 
 function getJugada(){
-
+	
 	let jugada= new Array();
-
+	
 	for(let numDado = 0; numDado<5; numDado++){
-
+		
 		jugada.push( getDado() );
 	}
-
+	
 	return jugada;	
 }
 
 
 
 function getCantidades(jugada){
-
+	
 	let cantidades= [0,0,0,0,0,0];
-
+	
 	jugada.forEach(
-					function( dado ){
-						cantidades[dado - 1]++;
+		function( dado ){
+			cantidades[dado - 1]++;
+			
+		}
+		);
+		
+		return cantidades;
+	}
+	
+	
+	function esGenerala(jugada){
+		return getCantidades(jugada).includes(5);
+	}
+	
+	function esPoker(jugada){
+		return getCantidades(jugada).includes(4);
+	}
+	
+	function esFull(jugada){
+		return getCantidades(jugada).includes(3) && getCantidades(jugada).includes(2);
+	}
+	
+	
+	
+	function jugar(){
 
-					}
-					);
+		let jugada = getJugada();
+			
+		let contenedorJugada = document.getElementById("jugada");
+			
+		let contenedorResultado = document.getElementById("resultado");
 
-	return cantidades;
-}
-
-
-function esGenerala(jugada){
-	return getCantidades(jugada).includes(5);
-}
-
-function esPoker(jugada){
-	return getCantidades(jugada).includes(4);
-}
-
-function esFull(jugada){
-	return getCantidades(jugada).includes(3) && getCantidades(jugada).includes(2);
-}
-
-
-
-let jugada = getJugada();
-
-let contenedorJugada = document.getElementById("jugada");
-
-let contenedorResultado = document.getElementById("resultado");
-
-
-
-
-
-for(let i=0; i < jugada.length; i++){
-
-	contenedorJugada.innerHTML +=' '+jugada[i]+' ';
-
-}
-
+		contenedorJugada.innerHTML='';
+		
+	
+		
+		
+		for(let i=0; i < jugada.length; i++){
+			
+			contenedorJugada.innerHTML +=' '+jugada[i]+' ';
+			
+		}
+		
+		
+		if (esGenerala(jugada)){
+			contenedorResultado.innerHTML = 'Tu resultado es : Hay generala GANASETE'
+		
+		}else if(esPoker(jugada)){
+			
+			contenedorResultado.innerHTML = 'Tu resultado es : Hay Poker GANASETE'
+		
+		
+		}else if(esFull(jugada)){
+			
+			contenedorResultado.innerHTML = 'Tu resultado es : Hay Full GANASETE'
+		
+		} else {
+			
+			contenedorResultado.innerHTML = 'Tu resultado es :PERDISTE'
+		
+		}
+	}
+	
 
 
 
@@ -72,23 +102,4 @@ console.log(jugada);
 console.log( getCantidades(jugada));
 
 
-
-if (esGenerala(jugada)){
-	console.log('Hay Generala');
-	contenedorResultado.innerHTML += 'Tu resultado es : Hay generala GANASETE'
-
-}else if(esPoker(jugada)){
-	console.log('Hay Poker');
-	contenedorResultado.innerHTML += 'Tu resultado es : Hay Poker GANASETE'
-
-
-}else if(esFull(jugada)){
-	console.log('Hay Full');
-	contenedorResultado.innerHTML += 'Tu resultado es : Hay Full GANASETE'
-
-} else {
-	console.log('No hay nada PERDISTE');
-	contenedorResultado.innerHTML += 'Tu resultado es :PERDISTE'
-
-}
 
